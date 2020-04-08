a90ffd4234bd0f871c6e10591785216303c65700
Thiago L. A. Miller <tmiller@mochsl.org.br>
2020-04-08 10:28:26 -0300

    FIX: worker.sh: Working with s3 and StorageGateway

--- sider/worker.sh
+++ sider/worker.sh
@@ -36,10 +36,16 @@ THREADS=10
 REFERENCE="$REFERENCE"
 ANNOTATION="$ANNOTATION"
 DATA_DIR="data"
+INPUT_TYPE="path"
+PREFIX="sider"
+MANIFEST="MANIFEST"
 OUTPUT_DIR="$(date +%Y-%m-%d-)$(hexdump -n 16 -v -e '/1 "%02X"' -e '/16 "\n"' /dev/urandom)"
 
-while getopts ":t:i:" OPTION; do
+while getopts ":t:i:o:r:a:p:" OPTION; do
 	case "$OPTION" in
+		i)
+			INPUT_TYPE="$OPTARG"
+			;;
 		o)
 			S3_OUTPUT="$OPTARG"
 			;;
@@ -52,6 +58,9 @@ while getopts ":t:i:" OPTION; do
 		a)
 			ANNOTATION="$OPTARG"
 			;;
+		p)
+			PREFIX="$OPTARG"
+			;;
 		?)
 			error "No such option '-$OPTARG'"
 			;;
@@ -59,8 +68,8 @@ while getopts ":t:i:" OPTION; do
 done
 
 shift $(($OPTIND - 1))
-[[ $# == 0 ]] && error "No BAM files passed"
-S3_INPUT_FILES=("$@")
+[[ $# == 0 ]] && error "No s3 sider list file passed"
+S3_MANIFEST="$1"
 
 save_results() {
 	log "Save results at $BASE/$OUTPUT_DIR to $S3_OUTPUT/$OUTPUT_DIR"
@@ -85,47 +94,60 @@ done
 log "WELCOME TO SIDER PIPELINE ON AWS"
 
 log "Create base directories"
-mkdir -p "$BASE/$OUTPUT_DIR" "$BASE/$DATA_DIR" \
+mkdir -p "$BASE/$OUTPUT_DIR" \
 	|| error "Failed to create base directories"
 
-PID=()
+aws s3 cp --quiet "$S3_MANIFEST" "$BASE/$MANIFEST" \
+	|| error "Failed to download '$S3_MANIFEST'"
 
-log "Download BAM files at '${S3_INPUT_FILES[*]}'"
-for path in "${S3_INPUT_FILES[@]}"; do
-	s3_path="s3://$path"
-	filename=$(basename "$path")
+case "$INPUT_TYPE" in
+	bucket)
+		log ">> Assets from s3 bucket <<"
 
-	log "Download $s3_path ..."
-	aws s3 cp --quiet "$s3_path" "$BASE/$DATA_DIR/" \
-		|| error "Failed to download file '$s3_path'"
+		log "Create '$BASE/$DATA_DIR' directory"
+		mkdir -p "$BASE/$DATA_DIR" \
+			|| error "Failed to create directory for s3 data"
 
-	log "Collating $BASE/$DATA_DIR/$filename ..."
-	samtools collate -f --output-fmt BAM -@ 2 \
-		-o "$BASE/$DATA_DIR/${filename%%.*}.sider.bam" \
-		"$BASE/$DATA_DIR/$filename" \
-		|| error "Failed to collate file '$BASE/$DATA_DIR/$filename'" &
+		log "Download BAM files at '$S3_MANIFEST'"
+		while read path; do
+			s3_path="$path"
 
-	PID+=($!)
-done
+			if [[ ! "$s3_path" =~ ^s3 ]]; then
+				s3_path="s3:/$path"
+			fi
 
-log "Waiting ..."
-for pid in "${PID[@]}"; do wait $pid; done
+			log "Download $s3_path ..."
+			aws s3 cp --quiet "$s3_path" "$BASE/$DATA_DIR/" \
+				|| error "Failed to download file '$s3_path'"
+		done < "$BASE/$MANIFEST"
 
-log "Create sider list from files at '$BASE/$DATA_DIR'"
-ls "$BASE/$DATA_DIR"/*.sider.bam > $BASE/sider_list.txt \
-	|| error "Failed to create sider_list.txt"
+		log "Create sider list from files at '$BASE/$DATA_DIR'"
+		ls "$BASE/$DATA_DIR"/* | grep '\.\(bam\|cram\|sam\)$' > "$BASE/sider_list.txt" \
+			|| error "Failed to create sider_list.txt"
+		;;
+	path)
+		log ">> Assets from mounted path <<"
+
+		log "Create sider_list.txt"
+		while read path; do
+			echo "$path" >> "$BASE/sider_list.txt" \
+				|| error "Failed to create sider_list.txt"
+		done < "$BASE/$MANIFEST"
+		;;
+	*)
+		error "Invalid '$INPUT_TYPE' [bucket|path]"
+		;;
+esac
 
 log "Run 'process-sample' step ..."
 sider ps \
-	-s \
-	-c 20000000 \
+	-c 2000000 \
 	-o "$BASE/$OUTPUT_DIR" \
-	-p sider \
+	-p "$PREFIX" \
 	-t $THREADS \
 	-m 15000 \
 	-F 0.9 \
 	-Q 20 \
-	-M 0.75 \
 	-l "$BASE/$OUTPUT_DIR/ps.log" \
 	-a "$ANNOTATION" \
 	-i "$BASE/sider_list.txt" \
@@ -136,15 +158,13 @@ sider mc \
 	-c 20000000 \
 	-e 500 \
 	-m 20 \
-	-x 1000000 \
 	-g 5 \
 	-n 3 \
-	-d \
 	-l "$BASE/$OUTPUT_DIR/mc.log" \
 	-t $THREADS \
 	-B "$ANNOTATION" \
 	-Q 20 \
-	-I "$BASE/$OUTPUT_DIR/sider.db" \
+	-I "$BASE/$OUTPUT_DIR/$PREFIX.db" \
 	|| error "Failed to run 'merge-call' step"
 
 log "Run 'make-vcf' step ..."
@@ -152,6 +172,6 @@ sider vcf \
 	-r "$REFERENCE" \
 	-l "$BASE/$OUTPUT_DIR/vcf.log" \
 	-o "$BASE/$OUTPUT_DIR" \
-	-p sider \
-	"$BASE/$OUTPUT_DIR/sider.db" \
+	-p "$PREFIX" \
+	"$BASE/$OUTPUT_DIR/$PREFIX.db" \
 	|| error "Failed to run 'make-vcf' step"
